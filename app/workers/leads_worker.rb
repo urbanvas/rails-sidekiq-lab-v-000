d2bd2ccabb8e5bc0875a7dfe60e5d8281129e960
Neil Seejoor <nvseejoor@gmail.com>
2019-03-04 19:48:47 +0000

    Automatically backed up by Learn

--- app/workers/leads_worker.rb
+++ app/workers/leads_worker.rb
@@ -4,7 +4,7 @@ class SongsWorker
 
   def perform(leads_file)
     CSV.foreach(leads_file, headers: true) do |lead|
-      Customer.create(email: lead[0], first_name: lead[1], last_name: lead[2])
+      Song.create(email: lead[0], first_name: lead[1], last_name: lead[2])
     end
   end
 end
